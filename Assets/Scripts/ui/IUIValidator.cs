c09a37e47aacfac2ab1d4ee4744e0e775d0d4cc4
XainFaith <XainFaith@gmail.com>
2021-12-10 12:43:38 -0800

    Removed Validation Interface and opted to use delegates for better control

--- Assets/Scripts/ui/IUIValidator.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UIElements;
-
-public interface IUIValidator
-{
-    public bool Validate(VisualElement caller);
-}
